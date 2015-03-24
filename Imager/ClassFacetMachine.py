Correct weighting

Imager/ClassFacetMachine.py
@@ -111,6 +111,11 @@ class ClassFacetMachine():
         _,NpixPaddedGrid=EstimateNpix(NpixFacet,Padding=Padding)
         self.NChanGrid=1
         self.PaddedGridShape=(self.NChanGrid,self.npol,NpixPaddedGrid,NpixPaddedGrid)
+        print>>log,"Sizes (%i x %i facets):"%(NFacets,NFacets)
+        print>>log,"   - Main field :   [%i x %i] pix"%(self.Npix,self.Npix)
+        print>>log,"   - Each facet :   [%i x %i] pix"%(NpixFacet,NpixFacet)
+        print>>log,"   - Padded-facet : [%i x %i] pix"%(NpixPaddedGrid,NpixPaddedGrid)
+        
         #self.setWisdom()
         self.SumWeights=np.zeros((self.NChanGrid,self.npol),float)
 
@@ -209,10 +214,12 @@ class ClassFacetMachine():
 
 
     def InitSerial(self):
+        if self.ConstructMode=="Fader":
+            SpheNorm=False
         for iFacet in sorted(self.DicoImager.keys()):
             GridMachine=ClassDDEGridMachine.ClassDDEGridMachine(self.GD,RaDec=self.DicoImager[iFacet]["RaDec"],
                                                                 lmShift=self.DicoImager[iFacet]["lmShift"],
-                                                                IdSharedMem=self.IdSharedMem,IDFacet=iFacet,
+                                                                IdSharedMem=self.IdSharedMem,IDFacet=iFacet,SpheNorm=SpheNorm,
                                                                 **self.DicoImager[iFacet]["DicoConfigGM"])
 
             self.DicoGridMachine[iFacet]["GM"]=GridMachine

Imager/ClassWeighting.py
@@ -14,8 +14,8 @@ def test():
     t=table(MS.MSName,ack=False)
     WEIGHT=t.getcol("IMAGING_WEIGHT")
     t.close()
-    ImShape=(1, 1, 1025, 1025)
-    CellSizeRad=(20./3600)*np.pi/180
+    ImShape=(1, 1, 257, 257)
+    CellSizeRad=(1./3600)*np.pi/180
     CW=ClassWeighting(ImShape,CellSizeRad)
     #CW.CalcWeights(MS.uvw[199:200],WEIGHT[199:200,0:3],MS.flag_all[199:200,0:3],MS.ChanFreq[0:3],Weighting="Uniform")
 
@@ -32,10 +32,10 @@ def test():
     #     CW.CalcWeights(uvw,W,flags,freqs,Weighting="Uniform")
 
     WEIGHT.fill(1)
-    MS.flag_all[MS.A0==MS.A1,:,:]=1
-    WEIGHT[MS.flag_all[:,:,0]==1]=0
+    MS.flag_all[MS.A0==MS.A1]=1
+    #WEIGHT[MS.flag_all[:,:,0]==1]=0
 
-    CW.CalcWeights(MS.uvw,WEIGHT,MS.flag_all,MS.ChanFreq,Robust=-1,Weighting="Briggs")
+    CW.CalcWeights(MS.uvw,WEIGHT,MS.flag_all,MS.ChanFreq,Robust=-1,Weighting="Uniform")
     
 
 class ClassWeighting():
@@ -53,8 +53,8 @@ class ClassWeighting():
         FOV=self.CellSizeRad*npixIm#/2
 
         #cell=1.5*4./(FOV)
-        cell=2./(FOV)
-        cell=4./(FOV)
+        cell=1./(FOV)
+        #cell=4./(FOV)
 
         #wave=6.
 
@@ -63,7 +63,9 @@ class ClassWeighting():
 
         d=np.sqrt(u**2+v**2)
         VisWeights[d==0]=0
-        uvmax=np.max(d)
+        Lmean=3e8/np.mean(freqs)
+
+        uvmax=np.max(d)/Lmean#(1./self.CellSizeRad)#/2#np.max(d)
         npix=2*(int(uvmax/cell)+1)
         if (npix%2)==0:
             npix+=1
@@ -72,7 +74,8 @@ class ClassWeighting():
         xc,yc=npix/2,npix/2
 
 
-
+        VisWeights=np.float64(VisWeights)
+        VisWeights.fill(1.)
 
 
         
@@ -82,6 +85,7 @@ class ClassWeighting():
             Mode=0
         elif Weighting=="Uniform":
             print>>log, "Weighting in Uniform mode"
+            print>>log, "Calculating imaging weights on an [%i,%i] grid"%(npix,npix)
             Mode=1
         elif Weighting=="Natural":
             print>>log, "Weighting in Natural mode"
@@ -92,17 +96,19 @@ class ClassWeighting():
         grid=np.zeros((npix,npix),dtype=np.float64)
 
 
-        VisWeights=np.float64(VisWeights)
-        flags=np.int32(flags)
-
+        flags=np.float32(flags)
+        WW=np.mean(1.-flags,axis=2)
+        VisWeights*=WW
+        
+        F=np.zeros(VisWeights.shape,np.int32)
         #print "u=",u
         #print "v=",v
         w=_pyGridder.pyGridderPoints(grid,
-                                     flags,
+                                     F,
                                      u,
                                      v,
                                      VisWeights,
-                                     2*float(Robust),
+                                     float(Robust),
                                      Mode,
                                      np.float32(freqs.flatten()),
                                      np.array([cell,cell],np.float64))
@@ -117,18 +123,21 @@ class ClassWeighting():
         # condx=((x>0)&(x<npix))
         # condy=((y>0)&(y<npix))
         # ind=np.where((condx & condy))[0]
-        # X=x[ind]
-        # Y=y[ind]
-
+        # X=x#[ind]
+        # Y=y#[ind]
+        
+        # w[w==0]=1e-10
         
         # import pylab
         # pylab.clf()
-        # pylab.scatter(uf.flatten(),vf.flatten(),c=w.flatten(),lw=0,alpha=0.3)#,w[ind,0])
-        # #pylab.imshow(w,interpolation="nearest")
-        # #pylab.scatter(X,Y)#,w[ind,0])
+        # #pylab.scatter(uf.flatten(),vf.flatten(),c=w.flatten(),lw=0,alpha=0.3,vmin=0,vmax=1)#,w[ind,0])
+        # grid[grid==0]=1e-10
+        # pylab.imshow(np.log10(grid),interpolation="nearest")
+        # incr=1
+        # pylab.scatter(X.ravel()[::incr],Y.ravel()[::incr],c=np.log10(w.ravel())[::incr],lw=0)#,alpha=0.3)
         # pylab.draw()
         # pylab.show(False)
         # pylab.pause(0.1)
-
+        # stop
         
         return w