e2fb85fc433ed1816979f1c53c566c39a65f1689
cyriltasse <cyril_tasse@yahoo.fr>
2015-04-02 17:54:19 +0200

    ok

--- Gridder/GridderSmear.h
+++ Gridder/GridderSmear.h
@@ -128,59 +128,87 @@ void ScaleJones(float complex* J0, float AlphaScaleJones){
 
 
 void MatInv(float complex *A, float complex* B, int H ){
-  float complex a,b,c,d,ff;
+  float complex ff;
 
-  if(H==0){
-      a=A[0];
-      b=A[1];
-      c=A[2];
-      d=A[3];}
-  else{
-    a=conj(A[0]);
-    b=conj(A[2]);
-    c=conj(A[1]);
-    d=conj(A[3]);
-  }  
-  ff=1./((a*d-c*b));
-  B[0]=ff*d;
-  B[1]=-ff*b;
-  B[2]=-ff*c;
-  B[3]=ff*a;
+  ff=1./((A[0]*A[3]-A[2]*A[1]));
+  B[0]=ff*A[3];
+  B[1]=-ff*A[1];
+  B[2]=-ff*A[2];
+  B[3]=ff*A[0];
 }
+
+/* void MatInv(float complex *A, float complex* B, int H ){ */
+/*   float complex a,b,c,d,ff; */
+
+/*   if(H==0){ */
+/*       a=A[0]; */
+/*       b=A[1]; */
+/*       c=A[2]; */
+/*       d=A[3];} */
+/*   else{ */
+/*     a=conj(A[0]); */
+/*     b=conj(A[2]); */
+/*     c=conj(A[1]); */
+/*     d=conj(A[3]); */
+/*   }   */
+/*   ff=1./((a*d-c*b)); */
+/*   B[0]=ff*d; */
+/*   B[1]=-ff*b; */
+/*   B[2]=-ff*c; */
+/*   B[3]=ff*a; */
+/* } */
 
 void MatH(float complex *A, float complex* B){
-  float complex a,b,c,d;
 
-  a=conj(A[0]);
-  b=conj(A[2]);
-  c=conj(A[1]);
-  d=conj(A[3]);
-  B[0]=a;
-  B[1]=b;
-  B[2]=c;
-  B[3]=d;
+  B[0]=conj(A[0]);
+  B[1]=conj(A[2]);
+  B[2]=conj(A[1]);
+  B[3]=conj(A[3]);
 }
+
+
+/* void MatH(float complex *A, float complex* B){ */
+/*   float complex a,b,c,d; */
+
+/*   a=conj(A[0]); */
+/*   b=conj(A[2]); */
+/*   c=conj(A[1]); */
+/*   d=conj(A[3]); */
+/*   B[0]=a; */
+/*   B[1]=b; */
+/*   B[2]=c; */
+/*   B[3]=d; */
+/* } */
 
 void MatDot(float complex *A, float complex* B, float complex* Out){
-  float complex a0,b0,c0,d0;
-  float complex a1,b1,c1,d1;
 
-  a0=A[0];
-  b0=A[1];
-  c0=A[2];
-  d0=A[3];
-  
-  a1=B[0];
-  b1=B[1];
-  c1=B[2];
-  d1=B[3];
-  
-  Out[0]=a0*a1+b0*c1;
-  Out[1]=a0*b1+b0*d1;
-  Out[2]=c0*a1+d0*c1;
-  Out[3]=c0*b1+d0*d1;
+  Out[0]=A[0]*B[0]+A[1]*B[2];
+  Out[1]=A[0]*B[1]+A[1]*B[3];
+  Out[2]=A[2]*B[0]+A[3]*B[2];
+  Out[3]=A[2]*B[1]+A[3]*B[3];
 
 }
+
+/* /\* void MatDot(float complex *A, float complex* B, float complex* Out){ *\/ */
+/* /\*   float complex a0,b0,c0,d0; *\/ */
+/* /\*   float complex a1,b1,c1,d1; *\/ */
+
+/* /\*   a0=A[0]; *\/ */
+/* /\*   b0=A[1]; *\/ */
+/* /\*   c0=A[2]; *\/ */
+/* /\*   d0=A[3]; *\/ */
+  
+/* /\*   a1=B[0]; *\/ */
+/* /\*   b1=B[1]; *\/ */
+/* /\*   c1=B[2]; *\/ */
+/* /\*   d1=B[3]; *\/ */
+  
+/* /\*   Out[0]=a0*a1+b0*c1; *\/ */
+/* /\*   Out[1]=a0*b1+b0*d1; *\/ */
+/* /\*   Out[2]=c0*a1+d0*c1; *\/ */
+/* /\*   Out[3]=c0*b1+d0*d1; *\/ */
+
+/* /\* } *\/ */
 
 static PyObject *pyTestMatrix(PyObject *self, PyObject *args)
 {
