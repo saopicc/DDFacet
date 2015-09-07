e78a726bf105e1f93e131e79e2c547aeaa9b975b
cyriltasse <cyril_tasse@yahoo.fr>
2015-09-08 00:19:42 +0200

    ok

--- Imager/ClassMultiScaleMachine.py
+++ Imager/ClassMultiScaleMachine.py
@@ -468,8 +468,8 @@ class ClassMultiScaleMachine():
 
 
         #self.SolveMode="MatchingPursuit"
-        #self.SolveMode="PI"
-        self.SolveMode="NNLS"
+        self.SolveMode="PI"
+        #self.SolveMode="NNLS"
 
         MeanFluxTrue=np.sum(FpolTrue.ravel()*self.DicoDirty["WeightChansImages"].ravel())
         if  self.SolveMode=="MatchingPursuit":
