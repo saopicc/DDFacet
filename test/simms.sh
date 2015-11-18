ec0c76b990682308b280c14dcc2060956dd46bb3
Oleg Smirnov <osmirnov@gmail.com>
2015-11-18 19:03:02 +0200

    added files for test dir

---
+++ test/simms.sh
@@ -0,0 +1,1 @@
+simms -T vla -t ascii -cs itrf -l test_vlac -dec 30d0m0s -ra 0h0m0s -st 4 -dt 10 -f0 1.4GHz -nc 4 -df 10kHz ~/projects/simms/simms/observatories/vlad.itrf.txt -pl "RR RL LR LL" -feed "perfect R L"
