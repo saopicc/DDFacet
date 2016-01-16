added channel slicing (fixes #27); revising weighting but it doesn't work properly yet

Data/ClassVisServer.py
@@ -18,19 +18,18 @@ import ClassJones
 def test():
     MSName="/media/tasse/data/killMS_Pack/killMS2/Test/0000.MS"
     VS=ClassVisServer(MSName,TVisSizeMin=1e8,Weighting="Natural")
-    VS.CalcWeigths((1,1,1000,1000),20.*np.pi/180)
+    VS.CalcWeights((1,1,1000,1000),20.*np.pi/180)
     VS.LoadNextVisChunk()
 
 class ClassVisServer():
     def __init__(self,MSName,GD=None,
                  ColName="DATA",
-                 Field=0,
                  TChunkSize=1,
                  TVisSizeMin=1,
                  DicoSelectOptions={},
                  LofarBeam=None,
                  AddNoiseJy=None,IdSharedMem="",
-                 Robust=2,Weighting="Briggs",NCPU=6):
+                 Robust=2,Weighting="Briggs",Super=1,NCPU=6):
 
         self.ReadOnce=False
         self.ReadOnce_AlreadyRead=False
@@ -54,12 +53,14 @@ class ClassVisServer():
         self.TVisSizeMin=TVisSizeMin
 
         self.Weighting=Weighting
+        self.Super=Super
         self.NCPU=NCPU
         self.VisWeights=None
         self.CountPickle=0
         self.ColName=ColName
-        self.Field = Field
-        self.TaQL = "FIELD_ID==%d"%Field 
+        self.Field = DicoSelectOptions.get("Field",0)
+        self.DDID = DicoSelectOptions.get("DDID",0)
+        self.TaQL = "FIELD_ID==%d&&DATA_DESC_ID==%d" % (self.Field, self.DDID)
         self.DicoSelectOptions=DicoSelectOptions
         self.SharedNames=[]
         self.PrefixShared=PrefixShared
@@ -86,8 +87,17 @@ class ClassVisServer():
         self.ListMS=[]
         self.ListGlobalFreqs=[]
         NChanMax=0
+        ChanStart = self.DicoSelectOptions.get("ChanStart",0)
+        ChanEnd   = self.DicoSelectOptions.get("ChanEnd",-1)
+        ChanStep  = self.DicoSelectOptions.get("ChanStep",1)
+        if (ChanStart,ChanEnd,ChanStep) == (0,-1,1):
+            chanslice = None
+        else:
+            chanslice = slice(ChanStart, ChanEnd if ChanEnd != -1 else None, ChanStep) 
         for MSName in self.ListMSName:
-            MS=ClassMS.ClassMS(MSName,Col=self.ColName,DoReadData=False,AverageTimeFreq=(1,3),Field=self.Field) 
+            MS=ClassMS.ClassMS(MSName,Col=self.ColName,DoReadData=False,AverageTimeFreq=(1,3),
+                Field=self.Field,DDID=self.DDID,
+                ChanSlice=chanslice) 
             self.ListMS.append(MS)
             self.ListGlobalFreqs+=MS.ChanFreq.flatten().tolist()
             
@@ -200,7 +210,7 @@ class ClassVisServer():
         self.OutImShape=OutImShape
         self.CellSizeRad=CellSizeRad
 
-    def CalcWeigths(self):
+    def CalcWeights(self):
         if self.VisWeights!=None: return
         #ImShape=self.PaddedFacetShape
         ImShape=self.FullImShape#self.FacetShape
@@ -220,7 +230,8 @@ class ClassVisServer():
 
         allweights = WeightMachine.CalcWeights(uvw,VisWeights,flags,self.MS.ChanFreq,
                                               Robust=Robust,
-                                              Weighting=self.Weighting)
+                                              Weighting=self.Weighting,
+                                              Super=self.Super)
 
         # self.WisWeights is a list of weight arrays, one per each MS in self.ListMS
         self.VisWeights = []
@@ -609,6 +620,7 @@ class ClassVisServer():
         WeightCol=self.GD["VisData"]["WeightCol"]
         # make lists of tables and row counts (one per MS)
         tabs = [ ms.GiveMainTable() for ms in self.ListMS ]
+        chanslices = [ ms.ChanSlice for ms in self.ListMS ]
         nrows = [ tab.nrows() for tab in tabs ]
         nr = sum(nrows)
         # preallocate arrays
@@ -619,12 +631,12 @@ class ClassVisServer():
 
         # now loop over MSs and read data
         row0 = 0
-        for num_ms, (nrow, tab) in enumerate(zip(nrows, tabs)):
+        for num_ms, (nrow, tab, chanslice) in enumerate(zip(nrows, tabs, chanslices)):
             uvws[row0:(row0+nrow),...]   = tab.getcol("UVW")
-            flags[row0:(row0+nrow),...] = tab.getcol("FLAG")
+            flags[row0:(row0+nrow),...] = tab.getcol("FLAG")[:,chanslice,:]
 
             if WeightCol == "WEIGHT_SPECTRUM":
-                WEIGHT=tab.getcol(WeightCol)
+                WEIGHT=tab.getcol(WeightCol)[:,chanslice]
                 print>>log, "  Reading column %s for the weights, shape is %s"%(WeightCol,WEIGHT.shape)
                 WEIGHT = (WEIGHT[:,:,0]+WEIGHT[:,:,3])/2.
             elif WeightCol == "WEIGHT":
@@ -635,13 +647,13 @@ class ClassVisServer():
                 WEIGHT = WEIGHT[:,np.newaxis] + np.zeros(self.MS.Nchan,np.float32)[np.newaxis,:]
             elif WeightCol == "WEIGHT+WEIGHT_SPECTRUM" or WeightCol == "WEIGHT_SPECTRUM+WEIGHT":
                 w = tab.getcol("WEIGHT")
-                ws = tab.getcol("WEIGHT_SPECTRUM")
+                ws = tab.getcol("WEIGHT_SPECTRUM")[:,chanslice]
                 print>>log, "  Reading column %s for the weights, shape is %s and %s"%(WeightCol,w.shape,ws.shape)
                 WEIGHT = w[:,np.newaxis,:] * ws
                 WEIGHT = (WEIGHT[:,:,0]+WEIGHT[:,:,3])/2.
             else:
                 ## in all other cases (i.e. IMAGING_WEIGHT) assume a column of shape NRow,NFreq to begin with, check for this:
-                WEIGHT=tab.getcol(WeightCol)
+                WEIGHT=tab.getcol(WeightCol)[:,chanslice]
                 print>>log, "  Reading column %s for the weights, shape is %s"%(WeightCol,WEIGHT.shape)
 
             if WEIGHT.shape != (nrow, self.MS.Nchan):

Imager/ClassDeconvMachine.py
@@ -105,19 +105,19 @@ class ClassImagerDeconv():
 
         self.VS=ClassVisServer.ClassVisServer(MSName,
                                               ColName=DC["VisData"]["ColName"],
-                                              Field=DC["VisData"]["Field"],
                                               TVisSizeMin=DC["VisData"]["TChunkSize"]*60,
                                               #DicoSelectOptions=DicoSelectOptions,
                                               TChunkSize=DC["VisData"]["TChunkSize"],
                                               IdSharedMem=self.IdSharedMem,
                                               Robust=DC["ImagerGlobal"]["Robust"],
                                               Weighting=DC["ImagerGlobal"]["Weighting"],
+                                              Super=DC["ImagerGlobal"]["Super"],
                                               DicoSelectOptions=dict(DC["DataSelection"]),
                                               NCPU=self.GD["Parallel"]["NCPU"],
                                               GD=self.GD)
         
         # self.VS.setFOV([1,1,1000,1000],[1,1,1000,1000],[1,1,1000,1000],2./3600.*np.pi/180)
-        # self.VS.CalcWeigths()
+        # self.VS.CalcWeights()
         # for i in range(10):
         #     print>>log, self.setNextData()
         # stop
@@ -142,9 +142,9 @@ class ClassImagerDeconv():
 
         self.InitFacetMachine()
         #self.VS.SetImagingPars(self.FacetMachine.OutImShape,self.FacetMachine.CellSizeRad)
-        #self.VS.CalcWeigths(self.FacetMachine.OutImShape,self.FacetMachine.CellSizeRad)
+        #self.VS.CalcWeights(self.FacetMachine.OutImShape,self.FacetMachine.CellSizeRad)
         self.VS.setFacetMachine(self.FacetMachine)
-        self.VS.CalcWeigths()
+        self.VS.CalcWeights()
 
 
 
@@ -519,10 +519,7 @@ class ClassImagerDeconv():
 
             MSName=self.VS.CurrentMS.MSName
             print>>log, "Writing predicted data in column %s of %s"%(PredictColName,MSName)
-            self.VS.CurrentMS.AddCol(PredictColName)
-            t=table(MSName,readonly=False,ack=False)
-            t.putcol(PredictColName,vis)
-            t.close()
+            self.VS.CurrentMS.PutVisColumn(PredictColName, vis)
             
 
     def setPSF(self):

Imager/ClassWeighting.py
@@ -9,17 +9,22 @@ log=MyLogger.getLogger("ClassWeighting")
 from DDFacet.Data import ClassMS
 from pyrap.tables import table
 
-def test():
-    MS=ClassMS.ClassMS("/media/6B5E-87D0/killMS2/TEST/Simul/0000.MS")
-    t=table(MS.MSName,ack=False)
-    WEIGHT=t.getcol("IMAGING_WEIGHT")
+def test(field=0,weight="Uniform"):
+    print>>log,"reading test MS"
+#    MS=ClassMS.ClassMS("/media/6B5E-87D0/killMS2/TEST/Simul/0000.MS")
+    MS=ClassMS.ClassMS("CYG-B-test.MS",DoReadData=False)
+    t=table(MS.MSName,ack=False).query("FIELD_ID==%d"%field)
+    WEIGHT=t.getcol("WEIGHT_SPECTRUM")
+    flag_all=t.getcol("FLAG")
+    uvw=t.getcol("UVW")
     t.close()
-    ImShape=(1, 1, 257, 257)
-    CellSizeRad=(1./3600)*np.pi/180
+    ImShape=(1, 1, 6125, 6125)
+    CellSizeRad=(0.06/3600)*np.pi/180
     CW=ClassWeighting(ImShape,CellSizeRad)
     #CW.CalcWeights(MS.uvw[199:200],WEIGHT[199:200,0:3],MS.flag_all[199:200,0:3],MS.ChanFreq[0:3],Weighting="Uniform")
 
-    MS.flag_all.fill(0)
+
+    flag_all.fill(0)
 
     # for i in [206]:#range(200,211):
     #     r0,r1=i,i+10
@@ -31,29 +36,111 @@ def test():
     #     freqs=MS.ChanFreq[0:3].copy()
     #     CW.CalcWeights(uvw,W,flags,freqs,Weighting="Uniform")
 
+    WEIGHT = WEIGHT.mean(axis=2)
     WEIGHT.fill(1)
-    MS.flag_all[MS.A0==MS.A1]=1
+    #flag_all[MS.A0==MS.A1]=1
     #WEIGHT[MS.flag_all[:,:,0]==1]=0
-
-    CW.CalcWeights(MS.uvw,WEIGHT,MS.flag_all,MS.ChanFreq,Robust=-1,Weighting="Uniform")
+    print>>log,"calculating test weights for shape %s"%(flag_all.shape,)
+    CW.CalcWeights(uvw,WEIGHT,flag_all,MS.ChanFreq,Robust=0,Weighting=weight)
     
+
+_cc = 299792458
 
 class ClassWeighting():
     def __init__(self,ImShape,CellSizeRad):
         self.ImShape=ImShape
         self.CellSizeRad=CellSizeRad
+
+    def CalcWeights(self,uvw,VisWeights,flags,freqs,Robust=0,Weighting="Briggs",Super=1):
+
+        nch,npol,npixIm,_ = self.ImShape
+        FOV = self.CellSizeRad*npixIm
+        cell =1./(Super*FOV)
+
+        npix = npixIm   # could be smarter and make it equal to uvmax/cell but why bother
+        # make even number of pixels in uv-weighting grid
+        if npixIm%2:
+            npix += 1
+        x0 = y0 = npix/2
+
+        # if any polarization is flagged, flag all 4 correlations
+        flags = flags.max(axis=2)
+        # zero weight to flagged points
+        VisWeights = VisWeights.astype(np.float64) * ~flags
         
-    def CalcWeights(self,uvw,VisWeights,flags,freqs,Robust=0,Weighting="Briggs"):
+        if Weighting=="Natural":
+            print>>log, "Weighting in Natural mode"
+            return VisWeights
+
+        # flip sign of negative v values -- we'll only grid the top half of the plane
+        uv = uvw[:,0:2].copy()
+        uv[ uv[:,1]<0 ] *= -1
+        # convert u/v to lambda, and then to pixel offset
+        uvl = uv[...,np.newaxis]*freqs[np.newaxis,np.newaxis,:]/_cc
+        uvl = np.floor(uvl/cell).astype(int)
+        x = uvl[:,0,:] + x0
+        y = uvl[:,1,:]
+        inbounds = (x<npix)&(y<npix/2)
+
+        # this is the only slow part
+        print>>log, "Calculating imaging weights on an [%i,%i] grid with cellsize %g (method 1)"%(npix,npix,cell)
+        grid = np.zeros(npix*npix/2+1,np.float64)
+        index = y*npix+x
+        index[~inbounds] = npix*npix/2
+        index_iter = zip(index,VisWeights)
+        def gridinc (dum,arg):
+           x,w = arg
+           grid[x] += w
+        reduce(gridinc,index_iter)
+        print>>log,"weight grid computed"
+
+        # print>>log, "Calculating imaging weights on an [%i,%i] grid with cellsize %g (method 2)"%(npix,npix,cell)
+        # # grid of weights. Only top half of uv-plane needs to be gridded
+        # grid = np.zeros((npix,npix/2),np.float64)
+        # # remember that y0 is 0 since it's only the top half that's gridded
+        # # this iterator gives us x,y and weight for each uv point on the grid
+        # xyw_iter = zip(x[inbounds],y[inbounds],VisWeights[inbounds])
+        # def gridinc (dum,arg):
+        #    x,y,w = arg
+        #    grid[x,y] += w
+        # reduce(gridinc,xyw_iter)
+        # print>>log,"weight grid computed"
+
+        if Weighting == "Uniform":
+            print>>log,"adjusting grid to uniform weight"
+            grid[grid!=0] = 1/grid[grid!=0]
+            print>>log,"applying grid"
+            grid[npix*npix/2] = 1
+            VisWeights *= grid[index]
+
+        elif Weighting == "Briggs":
+            grid[npix*npix/2] = 0
+            print>>log,"adjusting grid to briggs weight"
+            avgW = (grid**2).sum() / grid.sum()
+            numeratorSqrt = 5.0 * 10**(-Robust)
+            sSq = numeratorSqrt**2 / avgW
+            grid = 1/(1+grid*sSq)
+            print>>log,"applying grid"
+            grid[npix*npix/2] = 1
+            VisWeights *= grid[index]
+
+        print>>log,"weights computed"
+        return VisWeights
 
 
-        #u,v,_=uvw.T
+
+        
+    def CalcWeightsOld(self,uvw,VisWeights,flags,freqs,Robust=0,Weighting="Briggs",Super=1):
+
+
+        #u,v,_=uvw.T/*
 
         #Robust=-2
         nch,npol,npixIm,_=self.ImShape
         FOV=self.CellSizeRad*npixIm#/2
 
         #cell=1.5*4./(FOV)
-        cell=1./(FOV)
+        cell=1./(Super*FOV)
         #cell=4./(FOV)
 
         #wave=6.
@@ -63,10 +150,11 @@ class ClassWeighting():
 
         d=np.sqrt(u**2+v**2)
         VisWeights[d==0]=0
-        Lmean=3e8/np.mean(freqs)
-        Lmean=3e8/np.max(freqs)
+#        Lmean=3e8/np.mean(freqs)
+        Lmin=3e8/np.max(freqs)
 
-        uvmax=np.max(d)/Lmean#(1./self.CellSizeRad)#/2#np.max(d)
+        uvmax=np.max(d)/Lmin
+        #(1./self.CellSizeRad)#/2#np.max(d)
         npix=2*(int(uvmax/cell)+1)
         if (npix%2)==0:
             npix+=1
@@ -77,15 +165,15 @@ class ClassWeighting():
 
         VisWeights=np.float64(VisWeights)
         #VisWeights.fill(1.)
-
-
+        print>>log,"image grid cell is %g"%cell
         
         if Weighting=="Briggs":
-            print>>log, "Weighting in Briggs mode"
-            print>>log, "Calculating imaging weights with Robust=%3.1f on an [%i,%i] grid"%(Robust,npix,npix)
+            print>>log, "Weighting in Briggs mode (robust=%.1f, super=%.1f)"%(Robust,Super)
+            print>>log, "Calculating imaging weights on an [%i,%i] grid"%(npix,npix)
+            print>>log, ""
             Mode=0
         elif Weighting=="Uniform":
-            print>>log, "Weighting in Uniform mode"
+            print>>log, "Weighting in Uniform mode (super=%.1f)"%(Super)
             print>>log, "Calculating imaging weights on an [%i,%i] grid"%(npix,npix)
             Mode=1
         elif Weighting=="Natural":
@@ -113,6 +201,7 @@ class ClassWeighting():
                                      Mode,
                                      np.float32(freqs.flatten()),
                                      np.array([cell,cell],np.float64))
+        print>>log,"weights computed"
 
 
         # C=299792458.