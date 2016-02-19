added Super weighting parameter; fixed up various error reports; added checks for empty MS; improved progress bar in clean

Data/ClassVisServer.py
@@ -622,6 +622,15 @@ class ClassVisServer():
         tabs = [ ms.GiveMainTable() for ms in self.ListMS ]
         chanslices = [ ms.ChanSlice for ms in self.ListMS ]
         nrows = [ tab.nrows() for tab in tabs ]
+        for nr,ms in zip(nrows,self.ListMS):
+            if not nr:
+
+
+
+
+
+                print>>log,ModColor.Str("MS %s contains no data for this field and/or DDID"%(ms.MSName),col="red")
+                raise RuntimeError,"no data in MS %s"%ms.MSName
         nr = sum(nrows)
         # preallocate arrays
         # NB: this assumes nchan and ncorr is the same across all MSs in self.ListMS. Tough luck if it isn't!
@@ -632,7 +641,9 @@ class ClassVisServer():
         # now loop over MSs and read data
         row0 = 0
         for num_ms, (nrow, tab, chanslice) in enumerate(zip(nrows, tabs, chanslices)):
-            uvws[row0:(row0+nrow),...]   = tab.getcol("UVW")
+            if not nrow:
+                continue
+            uvws[row0:(row0+nrow),...]  = tab.getcol("UVW")
             flags[row0:(row0+nrow),...] = tab.getcol("FLAG")[:,chanslice,:]
 
             if WeightCol == "WEIGHT_SPECTRUM":
@@ -659,14 +670,17 @@ class ClassVisServer():
             if WEIGHT.shape != (nrow, self.MS.Nchan):
                 raise TypeError,"weights expected to have shape of %s"%((nrow, self.MS.Nchan),)
 
-            MeanW=np.mean(WEIGHT)
-            if MeanW!=0.:
-                WEIGHT/=MeanW
-
             weights[row0:(row0+nrow),...] = WEIGHT
 
             tab.close()
             row0 += nrow
+
+        # normalize weights
+        print>>log,"normalizing weights"
+        mw = weights.mean(dtype=np.float64)
+        if mw!=0.:
+            weights /= mw
+        print>>log,"done"
 
         return uvws,weights,flags,nrows
 

Imager/ClassWeighting.py
@@ -58,14 +58,17 @@ class ClassWeighting():
         FOV = self.CellSizeRad*npixIm
         cell =1./(Super*FOV)
 
+        print>>log,"applying flags to weights"
         # if any polarization is flagged, flag all 4 correlations
         flags = flags.max(axis=2)
         # zero weight to flagged points
         VisWeights = VisWeights.astype(np.float64) * ~flags
-        
+        # VisWeights *= ~flags
+
         if Weighting=="Natural":
             print>>log, "Weighting in Natural mode"
             return VisWeights
+        print>>log,"initializing weighting grid"
 
         # flip sign of negative v values -- we'll only grid the top half of the plane
         uv = uvw[:,0:2].copy()
@@ -119,16 +122,15 @@ class ClassWeighting():
         if Weighting == "Uniform":
 #            print>>log,"adjusting grid to uniform weight"
  #           grid[grid!=0] = 1/grid[grid!=0]
-            print>>log,"applying grid (uniform weighting)"
+            print>>log,("applying uniform weighting (super=%.2f)"%Super)
             VisWeights /= grid[index]
 
         elif Weighting == "Briggs":
-            print>>log,"adjusting grid to briggs weight"
+            print>>log,("applying briggs weighting (robust=%.2f, super=%.2f)"%(Robust, Super))
             avgW = (grid**2).sum() / grid.sum()
             numeratorSqrt = 5.0 * 10**(-Robust)
             sSq = numeratorSqrt**2 / avgW
             grid = 1/(1+grid*sSq)
-            print>>log,"applying grid"
             VisWeights *= grid[index]
 
         print>>log,"weights computed"