ffb6641acd8cf48982cc02d9807a075ca78995ba
Oleg Smirnov <osmirnov@gmail.com>
2016-03-31 17:12:28 +0200

    fixes #74, added single-letter options for all types of images

--- Imager/ClassDeconvMachine.py
+++ Imager/ClassDeconvMachine.py
@@ -344,7 +344,8 @@ class ClassImagerDeconv():
         #FacetMachinePSF.ToCasaImage(self.PSF,ImageName="%s.psf"%self.BaseName,Fits=True)
 
         self.FitPSF()
-        FacetMachinePSF.ToCasaImage(self.PSF,ImageName="%s.psf"%self.BaseName,Fits=True,beam=self.FWHMBeamAvg)
+        if "P" in self._saveims or "p" in self._saveims:
+            FacetMachinePSF.ToCasaImage(self.PSF,ImageName="%s.psf"%self.BaseName,Fits=True,beam=self.FWHMBeamAvg)
         
         FacetMachinePSF = None
 
@@ -449,7 +450,8 @@ class ClassImagerDeconv():
         
         # self.DicoDirty=self.FacetMachine.FacetsToIm()
 
-        self.FacetMachine.ToCasaImage(self.DicoDirty["MeanImage"],ImageName="%s.dirty"%self.BaseName,Fits=True)
+        if "d" in self._saveims:
+            self.FacetMachine.ToCasaImage(self.DicoDirty["MeanImage"],ImageName="%s.dirty"%self.BaseName,Fits=True)
 
         # ImageName="%s.dirty.MF"%self.BaseName
         # ImagData=self.DicoDirty["ImagData"]
@@ -459,7 +461,8 @@ class ClassImagerDeconv():
         # im.close()
 
 
-        self.FacetMachine.ToCasaImage(self.FacetMachine.NormImageReShape,ImageName="%s.NormFacets"%self.BaseName,Fits=True)
+        if "n" in self._saveims:
+            self.FacetMachine.ToCasaImage(self.FacetMachine.NormImageReShape,ImageName="%s.NormFacets"%self.BaseName,Fits=True)
 
         if self.DicoDirty["NormData"]!=None:
             #MeanCorr=self.DicoDirty["ImagData"]*self.DicoDirty["NormData"]
@@ -467,11 +470,15 @@ class ClassImagerDeconv():
             #MeanCorr=self.DicoDirty["ImagData"]*(self.DicoDirty["NormData"])
             nch,npol,nx,ny=MeanCorr.shape
             MeanCorr=np.mean(MeanCorr,axis=0).reshape((1,npol,nx,ny))
-            self.FacetMachine.ToCasaImage(MeanCorr,ImageName="%s.dirty.corr"%self.BaseName,Fits=True)
+            if "D" in self._saveims:
+                self.FacetMachine.ToCasaImage(MeanCorr,ImageName="%s.dirty.corr"%self.BaseName,Fits=True)
 
-            MeanNorm=np.mean(self.DicoDirty["NormData"],axis=0).reshape((1,npol,nx,ny))
-            self.FacetMachine.ToCasaImage(MeanNorm,ImageName="%s.Norm"%self.BaseName,Fits=True)
-        
+            self.MeanNormImage = np.mean(self.DicoDirty["NormData"],axis=0).reshape((1,npol,nx,ny))
+            if "N" in self._saveims:
+                self.FacetMachine.ToCasaImage(self.MeanNormImage,ImageName="%s.Norm"%self.BaseName,Fits=True)
+        else:
+            self.MeanNormImage = None
+
         #if self.VS.MultiFreqMode:
         #    for Channel in range(
 
@@ -554,7 +561,26 @@ class ClassImagerDeconv():
         if NMajor==None:
             NMajor=self.NMajor
 
+        # Oleg's "new" interface: set up which output images will be generated
+        # --SaveImages abc means save defaults plus abc
+        # --SaveOnly abc means only save abc
+        # --SaveImages all means save all
+        saveimages = self.GD["Images"]["SaveImages"]
+        saveonly = self.GD["Images"]["SaveOnly"]
+        if saveimages.lower() == "all" or saveonly.lower() == "all":
+            self._saveims = set([chr(x) for x in range(128)])  # all characters
+        else:
+            self._saveims = set(saveimages) | set(saveonly)
+        old_interface_saveims = self.GD["Images"]["SaveIms"] 
 
+        if "Model" in old_interface_saveims:
+            self._saveims.update("M")
+        if "Alpha" in old_interface_saveims:
+            self._saveims.update("A")
+        if "Model_i" in old_interface_saveims:
+            self._saveims.update("o")
+        if "Residual_i" in old_interface_saveims:
+            self._saveims.update("e")
 
         self.GiveDirty()
         self.setPSF()
@@ -684,11 +710,10 @@ class ClassImagerDeconv():
             else:
                 self.ResidImage=DicoImage["MeanImage"]
 
-            if "Residual_i" in self.GD["Images"]["SaveIms"]:
+            if "e" in self._saveims:
                 self.FacetMachine.ToCasaImage(DicoImage["MeanImage"],ImageName="%s.residual%2.2i"%(self.BaseName,iMajor),Fits=True)
 
-
-            if "Model_i" in self.GD["Images"]["SaveIms"]:
+            if "o" in self._saveims:
                 ModelImage=self.DeconvMachine.GiveModelImage(ThisMeanFreq)
                 self.FacetMachine.ToCasaImage(ModelImage,ImageName="%s.model%2.2i"%(self.BaseName,iMajor),Fits=True)
 
@@ -767,64 +792,99 @@ class ClassImagerDeconv():
         RefFreq=self.VS.RefFreq
         ModelMachine=self.DeconvMachine.ModelMachine
 
-
-
-        self.FacetMachine.ToCasaImage(self.ResidImage,ImageName="%s.residual"%(self.BaseName),Fits=True)
-
         # Putting back substracted componants
         if self.GD["DDESolutions"]["RestoreSub"]:
             try:
                 ModelMachine.PutBackSubsComps()
             except:
-                print>>log, ModColor.Str("Failed Putting back substracted componants")
+                print>>log, ModColor.Str("Failed Putting back substracted components")
 
 
         # model image
-        modelImage = None
-
         if self.GD["Images"]["MultiFreqMap"]:
-            modelImage=ModelMachine.GiveModelImage(np.array(
+            modelImage = ModelMachine.GiveModelImage(np.array(
                 [np.average(np.array(self.VS.FreqBandsInfos[band])) for band in self.VS.FreqBandsInfos]))
         else:
-            modelImage=ModelMachine.GiveModelImage(RefFreq) #Save only reference frequency model
+            modelImage = ModelMachine.GiveModelImage(RefFreq) #Save only reference frequency model
 
-        if "Model" in self.GD["Images"]["SaveIms"]:
-            self.FacetMachine.ToCasaImage(modelImage,ImageName="%s.model"%self.BaseName,Fits=True)
-        # restored image
-        self.RestoredImage = ModFFTW.ConvolveGaussian(modelImage,
-                                                      CellSizeRad=self.CellSizeRad,
-                                                      GaussPars=self.PSFGaussPars)
+        # do we have a non-trivial norm (i.e. DDE solutions or beam)?
+        # @cyriltasse: maybe there's a quicker way to check?
+        havenorm = self.MeanNormImage is not None and (self.MeanNormImage != 1).any()
 
-        assert self.RestoredImage.shape == self.ResidImage.shape, "Restored and residual images must have same shape"
-        self.RestoredImageRes=self.RestoredImage+self.ResidImage
-        self.FacetMachine.ToCasaImage(self.RestoredImageRes,
-                                      ImageName="%s.restored"%self.BaseName,
-                                      Fits=True,
-                                      beam=self.FWHMBeamAvg)
+        # make a dict of _images to save the intermediate images for when we need them
+        _images = {}
+        def sqrtnorm():
+            label = 'sqrtnorm'
+            if label not in _images:
+                _images[label] = np.sqrt(self.MeanNormImage) if havenorm else 1
+            return _images[label]
+        def appres():
+            return self.ResidImage
+        def intres():
+            label = 'intres'
+            if label not in _images:
+                _images[label] = intres = self.ResidImage/sqrtnorm() if havenorm else self.ResidImage 
+                intres[~np.isfinite(intres)] = 0
+            return _images[label]
+        def appmodel():
+            label = 'appmodel'
+            if label not in _images:
+                _images[label] = intmodel()*sqrtnorm() if havenorm else intmodel()
+            return _images[label]
+        def intmodel():
+            return modelImage
+        def appconvmodel():
+            label = 'appconvmodel'
+            if label not in _images:
+                _images[label] = ModFFTW.ConvolveGaussian(appmodel(),CellSizeRad=self.CellSizeRad,GaussPars=self.PSFGaussPars) \
+                                    if havenorm else intconvmodel()
+            return _images[label]
+        def intconvmodel():
+            label = 'intconvmodel'
+            if label not in _images:
+                _images[label] = ModFFTW.ConvolveGaussian(intmodel(),CellSizeRad=self.CellSizeRad,GaussPars=self.PSFGaussPars)
+            return _images[label]
 
+        # norm
+        if havenorm and ("S" in self._saveims or "s" in self._saveims):
+            self.FacetMachine.ToCasaImage(sqrtnorm(),ImageName="%s.fluxscale"%(self.BaseName),Fits=True)
+
+        # apparent-flux residuals
+        if "r" in self._saveims:
+            self.FacetMachine.ToCasaImage(appres(),ImageName="%s.app.residual"%(self.BaseName),Fits=True)
+        # intrinsic-flux residuals
+        if havenorm and "R" in self._saveims:
+            self.FacetMachine.ToCasaImage(intres(),ImageName="%s.int.residual"%(self.BaseName),Fits=True)
+
+        # apparent-flux model
+        if "m" in self._saveims:
+            self.FacetMachine.ToCasaImage(appmodel(),ImageName="%s.app.model"%self.BaseName,Fits=True)
+        # intrinsic-flux model
+        if havenorm and "M" in self._saveims:
+            self.FacetMachine.ToCasaImage(intmodel(),ImageName="%s.int.model"%self.BaseName,Fits=True)
+
+        # convolved-model image in apparent flux
+        if "c" in self._saveims:
+            self.FacetMachine.ToCasaImage(appconvmodel(),ImageName="%s.app.convmodel"%self.BaseName,Fits=True,beam=self.FWHMBeamAvg)
+        # convolved-model image in intrinsic flux
+        if havenorm and "C" in self._saveims: 
+            self.FacetMachine.ToCasaImage(intconvmodel(),ImageName="%s.int.convmodel"%self.BaseName,Fits=True,beam=self.FWHMBeamAvg)
+
+        # apparent-flux restored image
+        if "i" in self._saveims:
+            self.FacetMachine.ToCasaImage(appres()+appconvmodel(),ImageName="%s.app.restored"%self.BaseName,Fits=True,beam=self.FWHMBeamAvg)
+        # intrinsic-flux restored image
+        if havenorm and "I" in self._saveims:
+            self.FacetMachine.ToCasaImage(intres()+intconvmodel(),ImageName="%s.int.restored"%self.BaseName,Fits=True,beam=self.FWHMBeamAvg)
+        # mixed-flux restored image
+        if havenorm and "x" in self._saveims:
+            self.FacetMachine.ToCasaImage(appres()+intconvmodel(),ImageName="%s.restored"%self.BaseName,Fits=True,beam=self.FWHMBeamAvg)
+        
         # Alpha image
-        if ("Alpha" in self.GD["Images"]["SaveIms"])&(self.VS.MultiFreqMode):
+        if "A" in self._saveims and self.VS.MultiFreqMode:
             IndexMap=ModelMachine.GiveSpectralIndexMap(CellSizeRad=self.CellSizeRad,GaussPars=[self.PSFGaussParsAvg])
             # IndexMap=ModFFTW.ConvolveGaussian(IndexMap,CellSizeRad=self.CellSizeRad,GaussPars=[self.PSFGaussPars],Normalise=True)
             self.FacetMachine.ToCasaImage(IndexMap,ImageName="%s.alpha"%self.BaseName,Fits=True,beam=self.FWHMBeamAvg)
-
-
-        # self.RestoredImageRes=self.RestoredImage+self.ResidImage/np.sqrt(self.NormImage)
-        # self.FacetMachine.ToCasaImage(self.RestoredImageRes,ImageName="%s.restored.corr"%self.BaseName,Fits=True,beam=self.FWHMBeamAvg)
-
-        # self.FacetMachine.ToCasaImage(self.RestoredImage,ImageName="%s.modelConv"%self.BaseName,Fits=True,beam=self.FWHMBeamAvg)
-
-
-
-
-        
-        # pylab.clf()
-        # pylab.imshow(self.RestoredImage[0,0],interpolation="nearest")
-        # pylab.draw()
-        # pylab.show(False)
-        # pylab.pause(0.1)
-
-################################################
 
     def testDegrid(self):
         self.InitFacetMachine()
