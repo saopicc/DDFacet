f2aa2013b00a77a76502bf2b41c638d5744a7a44
Julien Girard <4m0nr3@gmail.com>
2016-09-05 12:48:33 +0200

    Refactor Clean, CleanSerial and CleanParallel of ClassImageDeconvMachineSSD to Deconvolve, DeconvolveSerial, DeconvolveParallel

--- Imager/ClassDeconvMachine.py
+++ Imager/ClassDeconvMachine.py
@@ -656,7 +656,7 @@ class ClassImagerDeconv():
             
             self.DeconvMachine.Update(DicoImage)
 
-            repMinor, continue_deconv, update_model = self.DeconvMachine.Clean()
+            repMinor, continue_deconv, update_model = self.DeconvMachine.Deconvolve()
             #self.DeconvMachine.ModelMachine.ToFile(self.DicoModelName) LB - Not sure this is necessary anymore
 
             ## returned with nothing done in minor cycle? Break out
