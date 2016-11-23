e34ca1a57cd51adcd9b7e61031c4457e0d63215b
cyriltasse <cyril_tasse@yahoo.fr>
2016-11-23 22:46:31 +0000

    ok

--- MaskDicoModel.py
+++ MaskDicoModel.py
@@ -4,8 +4,8 @@ import optparse
 import pickle
 import numpy as np
 from SkyModel.Other import MyLogger
-log=MyLogger.getLogger("MakeModel")
-from Sky import ClassSM
+log=MyLogger.getLogger("MaskDicoModel")
+
 try:
     from DDFacet.Imager.ModModelMachine import GiveModelMachine
 except:
@@ -13,17 +13,17 @@ except:
 from DDFacet.Imager import ClassCasaImage
 from pyrap.images import image
 
-SaveName="last_MakeModel.obj"
+SaveName="last_MaskDicoModel.obj"
 
 def read_options():
     desc="""Questions and suggestions: cyril.tasse@obspm.fr"""
     global options
-    opt = optparse.OptionParser(usage='Usage: %prog --ms=somename.MS <options>',version='%prog version 1.0',description=desc)
+    opt = optparse.OptionParser(usage='Usage: %prog <options>',version='%prog version 1.0',description=desc)
 
     group = optparse.OptionGroup(opt, "* Data-related options", "Won't work if not specified.")
-    group.add_option('--InDicoModel',help='List of targets [no default]',default='')
-    group.add_option('--OutDicoModel',help='List of targets [no default]',default='')
-    group.add_option('--MaskName',help='List of targets [no default]',default='')
+    group.add_option('--InDicoModel',help='Input DicoModel name [no default]',default='')
+    group.add_option('--OutDicoModel',help='Output DicoModel name [no default]',default='')
+    group.add_option('--MaskName',help='Name of the fits mask [no default]',default='')
     opt.add_option_group(group)
 
     options, arguments = opt.parse_args()
