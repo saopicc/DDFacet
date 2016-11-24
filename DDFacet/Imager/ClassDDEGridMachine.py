441a778e211e105474ded4e64237f77918d88a49
Oleg Smirnov <osmirnov@gmail.com>
2016-11-24 12:52:33 +0200

    more fixes to #259

--- DDFacet/Imager/ClassDDEGridMachine.py
+++ DDFacet/Imager/ClassDDEGridMachine.py
@@ -616,7 +616,7 @@ class ClassDDEGridMachine():
         vis = visIn
 
         T = ClassTimeIt.ClassTimeIt("put")
-        T.disable()
+        # T.disable()
         self.DoNormWeights = DoNormWeights
         if not(self.DoNormWeights):
             self.reinitGrid()
@@ -691,11 +691,12 @@ class ClassDDEGridMachine():
             ParamJonesList = ParamJonesList + LApplySol + LSumJones + LSumJonesChan + \
                 [np.float32(self.GD["DDESolutions"]["ReWeightSNR"])]
 
-        T2 = ClassTimeIt.ClassTimeIt("Gridder")
-        T2.disable()
+        # T2 = ClassTimeIt.ClassTimeIt("Gridder")
+        # T2.disable()
+        T.timeit("prep %d"%self.IDFacet)
 
         if self.GD["Compression"]["CompGridMode"] == 0:
-            raise RuntimeError("Depricated flag. Please use BDA gridder")
+            raise RuntimeError("Deprecated flag. Please use BDA gridder")
         else:
             OptimisationInfos = [
                 self.JonesType,
@@ -726,6 +727,8 @@ class ClassDDEGridMachine():
                                           OptimisationInfos,
                                           self.LSmear,
                                           np.int32(ChanMapping))
+
+            T.timeit("grid %d" % self.IDFacet)
 
     def CheckTypes(
         self,
