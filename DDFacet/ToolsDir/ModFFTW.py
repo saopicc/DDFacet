e54a4bb4302053149cb0a7e41f1a97f1849add40
Oleg Smirnov <osmirnov@gmail.com>
2017-02-01 19:22:59 +0200

    take abs() of FT(Gauss) to ensure convolution kernel is not offset

--- DDFacet/ToolsDir/ModFFTW.py
+++ DDFacet/ToolsDir/ModFFTW.py
@@ -318,6 +318,7 @@ def _convolveSingleGaussianFFTW(shareddict_path, field_in, field_out, ch, CellSi
         PSF /= np.sum(PSF)
     T.timeit("givegauss %d"%ch)
     fPSF = pyfftw.interfaces.numpy_fft.rfft2(iFs(PSF), overwrite_input=True, threads=1)
+    fPSF = np.abs(fPSF)
     npol = Ain.shape[0]
     for pol in range(npol):
         A = iFs(Ain[pol])
@@ -338,6 +339,7 @@ def _convolveSingleGaussianNP(shareddict_path, field_in, field_out, ch, CellSize
         PSF /= np.sum(PSF)
     T.timeit("givegauss %d"%ch)
     fPSF = np.fft.rfft2(PSF)
+    fPSF = np.abs(fPSF)
     npol = Ain.shape[0]
     for pol in range(npol):
         A = Ain[pol]
@@ -445,15 +447,16 @@ def ConvolveGaussianC(Ain0,CellSizeRad=None,GaussPars=[(0.,0.,0.)],Normalise=Fal
         # PSF=np.ones((Ain.shape[-1],Ain.shape[-1]),dtype=np.float32)
         if Normalise:
             PSF/=np.sum(PSF)
+        PSF = np.fft.ifftshift(PSF)
         fPSF = np.fft.fft2(PSF)
-        fPSF = fPSF.real
+        fPSF = np.abs(fPSF)
         for pol in range(npol):
             A = Ain[pol]
             fA = np.fft.fft2(A)
             nfA = fA*fPSF#Gauss
             if_fA = np.fft.ifft2(nfA)
             # if_fA=(nfA)
-            Aout[ch,pol,:,:] = if_fA.real
+            Aout[ch,pol,:,:] = np.fft.fftshift(if_fA.real)
         T.timeit("conv")
 
     return Aout
@@ -477,6 +480,7 @@ def ConvolveGaussianR (Ain0, CellSizeRad=None, GaussPars=[(0., 0., 0.)], Normali
             PSF /= np.sum(PSF)
         PSF = np.fft.ifftshift(PSF)
         fPSF = np.fft.rfft2(PSF)
+        fPSF = np.abs(fPSF)
         for pol in range(npol):
             fA = np.fft.rfft2(np.fft.ifftshift(Ain[pol]))
             nfA = fA * fPSF
