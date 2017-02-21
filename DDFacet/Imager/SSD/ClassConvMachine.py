03bedf2752da78373c187ae5877ed84de1533555
Cyril Tasse <cyril.tasse@nancep3.obs-nancay.fr>
2017-02-21 14:07:49 +0000

    ok

--- DDFacet/Imager/SSD/ClassConvMachine.py
+++ DDFacet/Imager/SSD/ClassConvMachine.py
@@ -309,8 +309,8 @@ class ClassConvMachine():
         T.timeit("2")
         for ich in range(NFreqBand):
             for ipol in range(npol):
-                #Conv[ich,ipol]=scipy.signal.fftconvolve(zAsq[ich,ipol], SubPSF[ich,ipol], mode='same')
-                Conv[ich,ipol]=ModFFTW.ConvolveFFTW2D(zAsq[ich,ipol], SubPSF[ich,ipol])
+                Conv[ich,ipol]=scipy.signal.fftconvolve(zAsq[ich,ipol], SubPSF[ich,ipol], mode='same')
+                #Conv[ich,ipol]=ModFFTW.ConvolveFFTW2D(zAsq[ich,ipol], SubPSF[ich,ipol])
                 
                 # import pylab
                 # pylab.subplot(1,3,1)
