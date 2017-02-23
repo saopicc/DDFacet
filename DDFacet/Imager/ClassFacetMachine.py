9442889c9c792d03f14b75307a46c0b2ccf1a1fc
Cyril Tasse <cyril.tasse@nancep3.obs-nancay.fr>
2017-02-23 18:08:24 +0000

    ok

--- DDFacet/Imager/ClassFacetMachine.py
+++ DDFacet/Imager/ClassFacetMachine.py
@@ -833,9 +833,12 @@ class ClassFacetMachine():
         # build Jones amplitude image
         DoCalcJonesNorm = NormJones and not "JonesNorm" in self._norm_dict
         if DoCalcJonesNorm:
-            self._norm_dict["JonesNorm"] = JonesNorm = self.FacetsToIm_Channel("Jones-amplitude")
-        else:
-            JonesNorm = self._norm_dict["JonesNorm"]
+            self._norm_dict["JonesNorm"] = self.FacetsToIm_Channel("Jones-amplitude")
+            _,npol,nx,ny=self._norm_dict["JonesNorm"].shape
+            self._norm_dict["MeanJonesNorm"] = np.mean(self._norm_dict["JonesNorm"],axis=0).reshape((1,npol,nx,ny))
+
+        self.JonesNorm = JonesNorm = self._norm_dict["JonesNorm"]
+        self.MeanJonesNorm = MeanJonesNorm = self._norm_dict["MeanJonesNorm"]
 
         # compute normalized per-band weights (WBAND)
         if self.VS.MultiFreqMode:
