9f1aa8ef239686b0faa2dedd6a88d8b9bc11c79f
rammanouil <ritaammanouil@gmail.com>
2017-12-05 15:15:21 +0100

    Muffin deconvMachine rest

--- DDFacet/Imager/ClassDeconvMachine.py
+++ DDFacet/Imager/ClassDeconvMachine.py
@@ -246,6 +246,12 @@ class ClassImagerDeconv():
                 from DDFacet.Imager.MORESANE import ClassImageDeconvMachineMoresane
                 self.DeconvMachine=ClassImageDeconvMachineMoresane.ClassImageDeconvMachine(MainCache=self.VS.maincache, **MinorCycleConfig)
                 print>>log,"Using MORESANE algorithm"
+            elif self.GD["Deconv"]["Mode"]=="MUFFIN":
+                if MinorCycleConfig["ImagePolDescriptor"] != ["I"]:
+                    raise NotImplementedError("Multi-polarization is not supported in MORESANE")
+                from DDFacet.Imager.MUFFIN import ClassImageDeconvMachineMUFFIN
+                self.DeconvMachine=ClassImageDeconvMachineMUFFIN.ClassImageDeconvMachine(MainCache=self.VS.maincache, **MinorCycleConfig)
+                print>>log,"Using MUFFIN algorithm"
             else:
                 raise NotImplementedError("Unknown --Deconvolution-Mode setting '%s'" % self.GD["Deconv"]["Mode"])
             self.ImageNoiseMachine.setMainCache(self.VS.maincache)
