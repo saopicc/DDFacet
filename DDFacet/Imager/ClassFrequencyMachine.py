Fixed Fpol normalisation bug

DDFacet/Imager/ClassFrequencyMachine.py
@@ -76,9 +76,9 @@ class ClassFrequencyMachine(object):
         :return: 
         """
         if self.nchan==1: #hack to deal with a single channel
-            self.Fit = lambda vals: vals
-            self.Eval = lambda vals: vals # this will just be the value in that channel
-            self.Eval_Degrid = lambda vals, Freqs: np.tile(vals, Freqs.size)
+            self.Fit = lambda vals, *args: vals
+            self.Eval = lambda vals, *args: vals # this will just be the value in that channel
+            self.Eval_Degrid = lambda vals, Freqs, *args: np.tile(vals, Freqs.size)
         else:
             if mode == "WSCMS":
                 # set order
@@ -389,10 +389,6 @@ class ClassFrequencyMachine(object):
 
     def solve_MAP(self, Iapp, A, W, Kinv, theta):
         Dinv = A.T.dot(W[:, None] * A) + np.diag(Kinv)
-        # this might be necessary if we order > Nband
-        # if np.linalg.cond(Dinv) > 1e17:
-        #     print "Had to add jitter"
-        #     Dinv += 1e-7*np.eye(self.order)
         res = np.linalg.solve(Dinv, A.T.dot(W * Iapp) + theta * Kinv)
         return res
 
@@ -424,12 +420,14 @@ class ClassFrequencyMachine(object):
             # incorporate stitched JonesNorm
             JonesFactor = np.sqrt(MeanJonesBand/JonesNorm)
             # scale I0 by beam at ref_freq
-            I0 = MaxDirty / BeamFactor[self.ref_freq_index]
+            I0 = MaxDirty / BeamFactor[self.ref_freq_index]/1.25  # 1.25 because its better to underestimate I0
+            nu_tmp = self.Freqsp[self.ref_freq_index]
+            print "I 0 = ", I0, 100*(nu_tmp/1530000000.0)**(-0.7), 100*(1480000000.0/1530000000.0)**(-0.7)
             # next compute the product of the averaging matrix and beam matrix
             ChanMappingGrid = self.PSFServer.DicoMappingDesc["ChanMappingGrid"]
             SAmat = np.zeros([self.nchan, self.nchan_degrid])
             for iCh in xrange(self.nchan):
-                I = np.argwhere(ChanMappingGrid[0] == iCh).squeeze()
+                I = np.argwhere(ChanMappingGrid[0] == iCh).squeeze()  # TODO - test on multiple MSs
                 nchunk = np.size(I)
                 SAmat[iCh, I] = BeamFactor[I]/(nchunk*JonesFactor[iCh])  # The division by JonesFactor corrects for the fact that the PSF is normalised
             self.SAX = SAmat.dot(self.Xdes)
@@ -437,7 +435,7 @@ class ClassFrequencyMachine(object):
             I0 = MaxDirty
         # get MFS weights
         W = self.PSFServer.DicoVariablePSF['SumWeights'].squeeze().astype(np.float64)
-        if I0>0.0:
+        if I0 > 0.0:
             theta = self.solve_MAP(Vals.astype(np.float64), self.SAX, W, self.prior_invcov/I0**2, I0*self.prior_theta)
         else:
             theta = self.solve_MAP(Vals.astype(np.float64), self.SAX, W, self.prior_invcov_neg/I0**2, I0*self.prior_theta_neg)

DDFacet/Imager/WSCMS/ClassImageDeconvMachineWSCMS.py
@@ -107,7 +107,7 @@ class ClassImageDeconvMachine():
         self.Freqs_degrid = np.asarray(AllDegridFreqs).flatten()
         self.SetPSF(kwargs["PSFVar"])
         self.setSideLobeLevel(kwargs["PSFAve"][0], kwargs["PSFAve"][1])
-        self.SetModelRefFreq(kwargs["RefFreq"])
+        #self.SetModelRefFreq(kwargs["RefFreq"])
         self.ModelMachine.setFreqMachine(self.Freqs, self.Freqs_degrid,
                                          weights=kwargs["PSFVar"]["WeightChansImages"], PSFServer=self.PSFServer)
 
@@ -475,9 +475,9 @@ class ClassImageDeconvMachine():
                     self.PSFServer.setLocation(x, y)  # Selects the facet closest to (x,y)
 
                     # Fit a polynomial to get coeffs (coeffs are for intrinsic flux)
-                    self.ModelMachine.Coeffs = self.ModelMachine.FreqMachine.FitPolyNew(Fpol[:, 0, 0, 0],
-                                                                                        JonesNorm[:, 0, 0, 0],
-                                                                                        self._MeanDirty[0, 0, x, y])
+                    self.ModelMachine.Coeffs = self.ModelMachine.FreqMachine.Fit(Fpol[:, 0, 0, 0],
+                                                                                 JonesNorm[:, 0, 0, 0],
+                                                                                 self._MeanDirty[0, 0, x, y])
 
                     # Overwrite with polynoimial fit (this returns the apparent flux)
                     Fpol[:, 0, 0, 0] = self.ModelMachine.FreqMachine.Eval(self.ModelMachine.Coeffs)

DDFacet/Imager/WSCMS/ClassModelMachineWSCMS.py
@@ -137,13 +137,13 @@ class ClassModelMachine(ClassModelMachinebase.ClassModelMachine):
 
         if Scale not in DicoComp.keys():
             DicoComp[Scale] = {}
+            DicoComp[Scale]["NumComps"] = 0
 
         if key not in DicoComp[Scale].keys():
             DicoComp[Scale][key] = {}
             DicoComp[Scale][key]["SolsArray"] = np.zeros(Sols.size, np.float32)
-            DicoComp[Scale][key]["NumComps"] = np.zeros(1, np.float32)
 
-        DicoComp[Scale][key]["NumComps"] += 1
+        DicoComp[Scale]["NumComps"] += 1  # to keep track of the number of components for a particularscale
         DicoComp[Scale][key]["SolsArray"] += Sols.ravel() * Gain
 
     def GiveModelImage(self, FreqIn=None, out=None):
@@ -173,6 +173,7 @@ class ClassModelMachine(ClassModelMachinebase.ClassModelMachine):
             zero_scale = self.ListScales[0]
 
         for scale in DicoComp.keys():
+            print>>log, "Found %i components at scale %f" % (DicoComp[scale]["NumComps"], scale)
             # Note here we are building a spectral cube delta function representation first and then convolving by
             # the scale at the end. Need to build intrinsic model
             ScaleModel = np.zeros((nchan, npol, nx, ny), dtype=np.float32)
@@ -289,6 +290,7 @@ class ClassModelMachine(ClassModelMachinebase.ClassModelMachine):
                 self.ConvPSFmean = PSFmean
                 # delta scale is not cleaned with the ConvPSF so these should all be unity
                 self.PSFFreqNormFactors = np.ones([self.Nchan, 1, 1, 1], dtype=np.float32)
+                self.FpolNormFactor = 1.0
 
             else:
                 self.ConvPSF, self.ConvPSFmean = self.ScaleMachine.GiveTwiceConvolvedPSF(iFacet, iScale)
@@ -299,13 +301,20 @@ class ClassModelMachine(ClassModelMachinebase.ClassModelMachine):
                 NormFactors = self.ScaleMachine.GivePSFFreqPeaks(self.CurrentFacet, self.CurrentScale)
 
                 # To normalise by the frequency response of ConvPSF implicitly contained in residual
-                # we need to keep track of the ConvPSF peaks
-                self.PSFFreqNormFactors = NormFactormean/NormFactors
+                # we need to keep track of the ConvPSF peaks. Note this is not done in wsclean but should give more
+                # even per band residuals
+                self.PSFFreqNormFactors = NormFactormean / NormFactors
 
                 # Finally normalise PSF by peak of ConvPSF0
                 # This would set the peak of ConvPSF0 to unity if we were cleaning
                 # the delta scale with the convolved PSF
                 self.ConvPSF /= self.ScaleMachine.ConvPSFNormFactor
+
+                # This normalisation for Fpol is required so that we don't see jumps between minor cycles.
+                # Basically since the PSF is normalised by this factor the components also need to be normalised
+                # by the same factor for the subtraction in the sub-minor cycle to be the same as the subtraction
+                # in the minor and major cycles.
+                self.FpolNormFactor = self.ScaleMachine.ConvPSFNormFactor
 
 
     # not working yet!!!
@@ -349,7 +358,6 @@ class ClassModelMachine(ClassModelMachinebase.ClassModelMachine):
         # determine most relevant scale. This is the most time consuming step hence the sub-minor cycle
         xscale, yscale, ConvMaxDirty, CurrentDirty, ConvDirtyCube, iScale,  = \
             self.ScaleMachine.do_scale_convolve(Dirty.copy(), meanDirty.copy())
-        print "ConvMaxDirty at start = ", ConvMaxDirty
         if iScale == 0:
             xscale = x
             yscale = y
@@ -370,10 +378,8 @@ class ClassModelMachine(ClassModelMachinebase.ClassModelMachine):
         # set twice convolve PSF for scale and facet if either has changed
         self.set_ConvPSF(self.PSFServer.iFacet, iScale)
 
-        # get the mask
-        # TODO - incorporate JonesNorm
-        self.auto_mask = True
-        if self.auto_mask:
+        # update scale dependent mask
+        if self.GD["WSCMS"]["AutoMask"]:
             mask = self.give_scale_mask(CurrentDirty.copy(), self.ConvPSFmean, self.CurrentGain)
             self.ScaleMachine.ScaleMaskArray[sigma] &= mask
             # import matplotlib.pyplot as plt
@@ -432,12 +438,11 @@ class ClassModelMachine(ClassModelMachinebase.ClassModelMachine):
             # Overwrite with polynoimial fit (Fpol is apparent flux)
             Fpol[:, 0, 0, 0] = self.FreqMachine.Eval(self.Coeffs)
 
-            # add model component to dictionary
-            self.AppendComponentToDictStacked((xscale, yscale), self.Coeffs, sigma, self.CurrentGain)
+            self.AppendComponentToDictStacked((xscale, yscale), self.Coeffs / self.FpolNormFactor, sigma, self.CurrentGain)
 
             # keep track of apparent model array (this is for subtraction in the upper minor loop)
             # Note apparent since we convolve with a normalised PSF
-            ScaleModel[:, 0, xscale, yscale] += self.CurrentGain * Fpol[:, 0, 0, 0]
+            ScaleModel[:, 0, xscale, yscale] += self.CurrentGain * Fpol[:, 0, 0, 0] / self.FpolNormFactor
 
             # Restore ConvPSF frequency response and subtract component from residual
             Fpol /= self.PSFFreqNormFactors
@@ -448,15 +453,16 @@ class ClassModelMachine(ClassModelMachinebase.ClassModelMachine):
             CurrentDirty[...] = np.sum(ConvDirtyCube * W.reshape((W.size, 1, 1, 1)), axis=0)[None, :, :, :]
 
             # find the peak
-            PeakMap = np.ascontiguousarray(CurrentDirty*self.ScaleMachine.ScaleMaskArray[sigma])
-            xscale, yscale, ConvMaxDirty = NpParallel.A_whereMax(PeakMap, NCPU=self.NCPU, DoAbs=self.DoAbs,
-                                                       Mask=None)
+            #PeakMap = np.ascontiguousarray(CurrentDirty*self.ScaleMachine.ScaleMaskArray[sigma])
+            xscale, yscale, ConvMaxDirty = NpParallel.A_whereMax(CurrentDirty, NCPU=self.NCPU, DoAbs=self.DoAbs,
+                                                                 Mask=None)
 
             # Update counters TODO - should add subminor cycle count to minor cycle count
             k += 1
             self.n_sub_minor_iter += 1
 
-        print "ConvMaxDirty at end = ", ConvMaxDirty
+        if ConvMaxDirty == 0.0:
+            print np.abs(CurrentDirty).max()
         # report if max sub-iterations exceeded
         if k >= self.ScaleMachine.NSubMinorIter:
             print>>log, "Maximum subiterations reached. "