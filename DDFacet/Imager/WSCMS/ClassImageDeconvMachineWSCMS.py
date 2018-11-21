Fixed issue with masking. Added scale dependent threshold. Tweaked defaults

DDFacet/Imager/WSCMS/ClassImageDeconvMachineWSCMS.py
@@ -99,16 +99,35 @@ class ClassImageDeconvMachine():
         self.CacheFileName = CacheFileName
         self.PSFHasChanged = False
 
+        #  TODO - use MaskMachine for this
         CleanMaskImage = self.GD["Mask"]["External"]
         if CleanMaskImage is not None:
             print>>log, "Reading mask image: %s"%CleanMaskImage
             MaskArray=image(CleanMaskImage).getdata()
-            nch,npol,_,_=MaskArray.shape
+            nch,npol,nxmask,nymask=MaskArray.shape
+            # if (nch > 1) or (npol > 1):
+            #     print>>log, "Warning - only single channel and pol mask supported. Will use mask for ch 0 pol 0"
+            # MaskArray = MaskArray[0,0]
+            # _, _, nxmod, nymod = self.ModelMachine.ModelShape
+            # if (nxmod != nxmask) or (nymod !=nymask):
+            #     print>>log, "Warning - shape of mask != shape of your model. Will pad/trncate to match model shape"
+            #     nxdiff = nxmod - nxmask
+            #     nydiff = nymod - nymask
+            #     if nxdiff < 0:
+            #         MaskArray = MaskArray
             self._MaskArray=np.zeros(MaskArray.shape,np.bool8)
             for ch in range(nch):
                 for pol in range(npol):
                     self._MaskArray[ch,pol,:,:]=np.bool8(1-MaskArray[ch,pol].T[::-1].copy())[:,:]
             self.MaskArray=np.ascontiguousarray(self._MaskArray)
+
+        # import matplotlib.pyplot as plt
+        # plt.imshow(self.MaskArray[0,0])
+        # plt.colorbar()
+        # plt.show()
+        #
+        # import sys
+        # sys.exit(0)
 
         self._peakMode = "normal"
 

DDFacet/Imager/WSCMS/ClassModelMachineWSCMS.py
@@ -357,7 +357,7 @@ class ClassModelMachine(ClassModelMachinebase.ClassModelMachine):
         x, y, MaxDirty = NpParallel.A_whereMax(meanDirty, NCPU=self.NCPU, DoAbs=self.DoAbs,
                                                Mask=self.ScaleMachine.MaskArray)
 
-        mask = np.zeros_like(meanDirty, dtype=np.bool)
+        mask = np.ones_like(meanDirty, dtype=np.bool)
 
         threshold = 0.9*np.abs(MaxDirty)
 
@@ -366,7 +366,7 @@ class ClassModelMachine(ClassModelMachinebase.ClassModelMachine):
         while i < maxit and np.abs(MaxDirty) > threshold:
             val = meanDirty[0, 0, x, y]
             # print i, val, gain*val*meanPSF[0,0,:,:].max(), val - gain*val*meanPSF[0,0,:,:].max()
-            mask[:, :, x, y] = 1
+            mask[:, :, x, y] = 0
             meanDirty = self.SubStep((x, y), gain*val*meanPSF, meanDirty)
             x, y, MaxDirty = NpParallel.A_whereMax(meanDirty, NCPU=self.NCPU, DoAbs=self.DoAbs,
                                                    Mask=self.ScaleMachine.MaskArray)
@@ -413,8 +413,8 @@ class ClassModelMachine(ClassModelMachinebase.ClassModelMachine):
                                         self.ScaleMachine.Conv2PSFmean[str(iScale)],
                                         self.CurrentGain)
             if str(iScale) not in self.ScaleMachine.ScaleMaskArray:
-                self.ScaleMachine.ScaleMaskArray[str(iScale)] = np.zeros_like(meanDirty, dtype=np.bool)
-            self.ScaleMachine.ScaleMaskArray[str(iScale)] |= mask
+                self.ScaleMachine.ScaleMaskArray[str(iScale)] = np.ones_like(meanDirty, dtype=np.bool)
+            self.ScaleMachine.ScaleMaskArray[str(iScale)] &= mask
             CurrentMask = self.ScaleMachine.ScaleMaskArray[str(iScale)].view()
         else:
             CurrentMask = self.ScaleMachine.MaskArray
@@ -426,12 +426,12 @@ class ClassModelMachine(ClassModelMachinebase.ClassModelMachine):
         # We cannot use StoppingFlux from minor cycle directly since the maxima of the scale convolved residual
         # is different from the maximum of the residual
         Threshold = self.ScaleMachine.PeakFactor * ConvMaxDirty
-        if Stopping_flux is not None:
-            if iScale:
-                DirtyRatio = ConvMaxDirty / MaxDirty
-                # TODO - set scale dependent threshold
-                Threshold = np.maximum(Threshold, Stopping_flux * DirtyRatio * 0.75)
-            else:
+        if iScale:
+            DirtyRatio = ConvMaxDirty / MaxDirty
+            # scale_fact = self.ScaleMachine.FWHMs[iScale]/self.ScaleMachine.FWHMs[0]
+            Threshold = Threshold * DirtyRatio * self.GD["WSCMS"]["ThresholdBias"] ** (iScale + 1)
+        else:
+            if Stopping_flux is not None:
                 Threshold = np.maximum(Threshold, Stopping_flux)
 
         # run subminor loop