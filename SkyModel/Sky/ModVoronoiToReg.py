c19a492988a47ce4e23810a2378ceb3946666b93
cyriltasse <cyril_tasse@yahoo.fr>
2019-11-27 12:37:56 +0000

    ok

--- SkyModel/Sky/ModVoronoiToReg.py
+++ SkyModel/Sky/ModVoronoiToReg.py
@@ -4,9 +4,11 @@ from SkyModel.Other import ModCoord
 import numpy as np
 from scipy.spatial import Voronoi
 import ModVoronoi
-from SkyModel.Other import MyLogger
 from SkyModel.Other import ModColor
-log=MyLogger.getLogger("VoronoiToReg")
+from DDFacet.Other import logger
+log = logger.getLogger("VoronoiToReg")
+#from SkyModel.Other import MyLogger
+#log=MyLogger.getLogger("VoronoiToReg")
 
 class VoronoiToReg():
     def __init__(self,rac,decc):
