c80f42d16d9833363d939e0e02c0abb1ab62f9e9
cyriltasse <cyril_tasse@yahoo.fr>
2022-03-23 11:57:50 +0000

    ok

--- DDFacet/Tests/FindDiffsCache.py
+++ DDFacet/Tests/FindDiffsCache.py
@@ -4,14 +4,20 @@ Useful test function to debug two branches that does the diff of two caches
 import numpy as np
 import os
 import glob
+from DDFacet.Other import ModColor
 
-C0="ssd0000.tsel.MS.keep.F0.D0.ddfcache"
-C1="ssd0000.tsel.MS.F0.D0.ddfcache"
-#SubDirList=[x[0] for x in os.walk(C0)]
+
 
 def test():
-    CheckDir(C0,C1)
+    
+    ll=sorted(glob.glob("G55*.ddfcache"))
+    for l in ll:
+        C0=l
+        C1="%s.master"%l
+        CheckDir(C0,C1)
 
+
+        
 def CheckDir(R0,R1):
     print("===============================================")
     LFiles=glob.glob("%s/*"%R0)
@@ -25,6 +31,7 @@ def CheckDir(R0,R1):
             CheckDir(F0,F1)
         else:
             if os.path.getsize(F0)==0 or os.path.getsize(F1)==0: continue
+            if "CF" in F0 or "Dirty" in F0: continue
             if F0[-5:]==".hash":
                 pass
                 #os.system("diff %s %s"%(F0,F1))
@@ -40,12 +47,31 @@ def CheckDir(R0,R1):
                         if d0.size==0:
                             print("   %s: zero size"%k)
                         else:
-                            print("   %s:"%k,np.abs(d0-d1).max())
+                            diff=np.abs(d0-d1).max()
+                            if diff>0:
+                                P=lambda s: print(ModColor.Str(s))
+                            else:
+                                P=print
+                            P("   %s: %e"%(k,diff))
+                            
                 elif D0.dtype==np.bool:
                     D0=np.float32(D0)
                     D1=np.float32(D1)
+                    diff=np.abs(D0-D1).max()
+                    if diff==0: continue
                     print("Array diffs: %s.%s - %s.%s"%(str(D0.dtype),str(D0.shape),str(D1.dtype),str(D1.shape)))
-                    print("         ",np.abs(D0-D1).max())
+                    if diff>0:
+                        P=lambda s: print(ModColor.Str(s))
+                    else:
+                        P=print
+                    P("  %e"%diff)
                 else:
+                    diff=np.abs(D0-D1).max()
+                    if diff==0: continue
                     print("Array diffs: %s.%s - %s.%s"%(str(D0.dtype),str(D0.shape),str(D1.dtype),str(D1.shape)))
-                    print("         ",np.abs(D0-D1).max())
+                    if diff>0:
+                        P=lambda s: print(ModColor.Str(s))
+                    else:
+                        P=print
+                    P("  %e"%diff)
+
