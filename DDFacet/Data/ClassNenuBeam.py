472d6785a8d6774c947a2960e0089a96541258a8
ebonnassieux <etienne.bonnassieux@unibo.it>
2022-04-21 12:20:00 +0200

    beam is coded: needs validating

--- DDFacet/Data/ClassNenuBeam.py
+++ DDFacet/Data/ClassNenuBeam.py
@@ -22,14 +22,31 @@ from DDFacet.Other import logger
 log= logger.getLogger("ClassNenuBeam")
 from DDFacet.Other import ClassTimeIt
 from DDFacet.Other import ModColor
+from astropy.time  import Time, TimeDelta
 
 # NENUFAR DEPENDENCIES
-from nenupy.instru import MiniArray, NenuFAR, Polarization, NenuFAR_Configuration
-from nenupy.astro.pointing import Pointing
-
-
-
+try:
+    from nenupy.instru         import MiniArray, NenuFAR, Polarization, NenuFAR_Configuration, miniarrays_rotated_like
+    from nenupy.astro.pointing import Pointing
+    from nenupy.astro.sky      import Sky
+    from nenupy.astro.target   import FixedTarget
+    from astropy.coordinates   import SkyCoord
+except ImportError:
+    print("The DDFacet implementation of the NenuFAR beam response")
+    print("relies on the nenupy library, which is not installed by")
+    print("default. You can install it via pypy as follows:")
+    print("")
+    print("pip3 install nenupy")
+    print("")
+    print("For more information, see: https://nenupy.readthedocs.io/en/latest/install.html")
 import numpy as np
+import astropy.units as u
+
+
+    
+# be bad
+import warnings
+warnings.filterwarnings("ignore")#, category=DeprecationWarning) 
 
 def AngDist(ra0,dec0,ra1,dec1):
     AC=np.arccos
@@ -50,21 +67,7 @@ class ClassNenuBeam():
         self.MS=MS
         self.SR=None
         self.CalcFreqDomains()
-
-        # define pointing
-        #self.pointing=Pointing.target_tracking()
-
-        # define array, reading from the dataset
-
-
-        print("farts")
-        stop
-
-        # calculate beam response
-
-
         
-
     def getBeamSampleTimes(self,times, **kwargs):
         DtBeamMin = self.GD["DtBeamMin"]
         DtBeamSec = DtBeamMin*60
@@ -80,187 +83,80 @@ class ClassNenuBeam():
     def CalcFreqDomains(self):
         ChanWidth=self.MS.ChanWidth.ravel()[0]
         ChanFreqs=self.MS.ChanFreq.flatten()
-
         NChanJones=self.GD["NBand"]
         if NChanJones==0:
             NChanJones=self.MS.NSPWChan
         ChanEdges=np.linspace(ChanFreqs.min()-ChanWidth/2.,ChanFreqs.max()+ChanWidth/2.,NChanJones+1)
-
         FreqDomains=[[ChanEdges[iF],ChanEdges[iF+1]] for iF in range(NChanJones)]
         FreqDomains=np.array(FreqDomains)
         self.FreqDomains=FreqDomains
         self.NChanJones=NChanJones
-
         MeanFreqJonesChan=(FreqDomains[:,0]+FreqDomains[:,1])/2.
         DFreq=np.abs(self.MS.ChanFreq.reshape((self.MS.NSPWChan,1))-MeanFreqJonesChan.reshape((1,NChanJones)))
         self.VisToJonesChanMapping=np.argmin(DFreq,axis=1)
 
-        self.calcCoefs()
-
-        
-    def calcCoefs(self):
-        ChanFreqs=self.MS.ChanFreq.flatten()
-        DicoCoefs={}
-
-        self.ATCABeamType="csiro"
-
-        if self.ATCABeamType=="csiro":
-            # coefficients for 16cm band of the ATCA, taken from the "+" values of  Tab. 3 in
-            # Sec. 4.1.4 of https://www.narrabri.atnf.csiro.au/people/ste616/beamshapes/beamshape_16cm.html
-            # coded by Etienne Bonnassieux. Bandwidth for each of these bands is 128 MHz except for
-            # edges. Fit valid only up to 100 arcmin.
-            DicoCoefs[0]={"Name":"ATCA-16-0", "f0":1268.e6,
-                          "C":np.array([1,0,-1.154e-03,0,+5.351e-07,0,-1.265e-10,0,
-                                        +1.634e-14,0,-1.099e-18,0,+3.009e-23])}
-            DicoCoefs[1]={"Name":"ATCA-16-1", "f0":1396.e6,
-                          "C":np.array([1,0,-1.092e-03,0,+4.660e-07,0,-9.875e-11,0,
-                                        +1.119e-14,0,-6.504e-19,0,+1.522e-23])}
-            DicoCoefs[2]={"Name":"ATCA-16-2", "f0":1524.e6,
-                          "C":np.array([1,0,-1.048e-03,0,+4.305e-07,0,-8.824e-11,0,
-                                        +9.684e-15,0,-5.446e-19,0,+1.232e-23])}
-            DicoCoefs[3]={"Name":"ATCA-16-3", "f0":1652.e6,
-                          "C":np.array([1,0,-1.033e-03,0,+4.221e-07,0,-8.692e-11,0,
-                                        +9.679e-15,0,-5.571e-19,0,+1.298e-23])}
-            DicoCoefs[4]={"Name":"ATCA-16-4", "f0":1780.e6,
-                          "C":np.array([1,0,-1.003e-03,0,+4.016e-07,0,-8.170e-11,0,
-                                        +9.046e-15,0,-5.199e-19,0,+1.213e-23])}
-            DicoCoefs[5]={"Name":"ATCA-16-5", "f0":1908.e6,
-                          "C":np.array([1,0,-9.794e-04,0,+3.831e-07,0,-7.562e-11,0,
-                                        +8.067e-15,0,-4.440e-19,0,+9.882e-24])}
-            DicoCoefs[6]={"Name":"ATCA-16-6", "f0":2036.e6,
-                          "C":np.array([1,0,-9.692e-04,0,+3.848e-07,0,-7.809e-11,0,
-                                        +8.621e-15,0,-4.927e-19,0,+1.140e-23])}
-            DicoCoefs[7]={"Name":"ATCA-16-7", "f0":2164.e6,
-                          "C":np.array([1,0,-9.919e-04,0,+4.028e-07,0,-8.251e-11,0,
-                                        +9.077e-15,0,-5.122e-19,0,+1.163e-23])}
-            DicoCoefs[8]={"Name":"ATCA-16-8", "f0":2292.e6,
-                          "C":np.array([1,0,-1.007e-03,0,+4.131e-07,0,-8.480e-11,0,
-                                        +9.305e-15,0,-5.220e-19,0,+1.176e-23])}
-            DicoCoefs[9]={"Name":"ATCA-16-9", "f0":2420.e6,
-                          "C":np.array([1,0,-1.016e-03,0,+4.235e-07,0,-8.817e-11,0,
-                                        +9.772e-15,0,-5.521e-19,0,+1.251e-23])}
-            DicoCoefs[10]={"Name":"ATCA-16-10", "f0":2548.e6,
-                           "C":np.array([1,0,-1.033e-03,0,+4.375e-07,0,-9.223e-11,0,
-                                         +1.033e-14,0,-5.885e-19,0,+1.343e-23])}
-            DicoCoefs[11]={"Name":"ATCA-16-11", "f0":2676.e6,
-                           "C":np.array([1,0,-1.035e-03,0,+4.395e-07,0,-9.269e-11,0,
-                                         +1.038e-14,0,-5.919e-19,0,+1.353e-23])}
-            DicoCoefs[12]={"Name":"ATCA-16-12", "f0":2804.e6,
-                           "C":np.array([1,0,-1.064e-03,0,+4.622e-07,0,-9.913e-11,0,
-                                         +1.125e-14,0,-6.502e-19,0,+1.505e-23])}
-            DicoCoefs[13]={"Name":"ATCA-16-13", "f0":2932.e6,
-                           "C":np.array([1,0,-1.081e-03,0,+4.769e-07,0,-1.045e-10,0,
-                                         +1.222e-14,0,-7.325e-19,0,+1.768e-23])}
-        if self.ATCABeamType=="gaussian":
-            DicoCoefs[0] ={"f0":1.000e9,"f1":1.332e9,"C":np.array([46.53])}
-            DicoCoefs[1] ={"f0":1.332e9,"f1":1.460e9,"C":np.array([47.58])}
-            DicoCoefs[2] ={"f0":1.460e9,"f1":1.588e9,"C":np.array([48.91])}
-            DicoCoefs[3] ={"f0":1.588e9,"f1":1.716e9,"C":np.array([49.35])}
-            DicoCoefs[4] ={"f0":1.716e9,"f1":1.844e9,"C":np.array([50.05])}
-            DicoCoefs[5] ={"f0":1.844e9,"f1":1.972e9,"C":np.array([50.70])}
-            DicoCoefs[6] ={"f0":1.972e9,"f1":2.100e9,"C":np.array([51.22])}
-            DicoCoefs[7] ={"f0":2.100e9,"f1":2.228e9,"C":np.array([50.61])}
-            DicoCoefs[8] ={"f0":2.228e9,"f1":2.356e9,"C":np.array([50.01])}
-            DicoCoefs[9] ={"f0":2.356e9,"f1":2.484e9,"C":np.array([49.66])}
-            DicoCoefs[10]={"f0":2.484e9,"f1":2.612e9,"C":np.array([49.61])}
-            DicoCoefs[11]={"f0":2.612e9,"f1":2.740e9,"C":np.array([49.48])}
-            DicoCoefs[12]={"f0":2.740e9,"f1":2.868e9,"C":np.array([48.81])}
-            DicoCoefs[13]={"f0":2.868e9,"f1":3.996e9,"C":np.array([48.31])}
-
-        NBand=len(DicoCoefs)
-
-                
-        chanBand0=np.zeros((ChanFreqs.size),dtype=int)
-        chanBand1=np.zeros((ChanFreqs.size),dtype=int)
-        xNull=np.zeros((ChanFreqs.size,),np.float32)
-        yNull=np.zeros((ChanFreqs.size,),np.float32)
-        beamfreq0=np.zeros((ChanFreqs.size,),np.float32)
-        for ich in range(ChanFreqs.size):
-            f=ChanFreqs[ich]
-            NoMatch=True
-            # by default, set the band ranges to be the biggest available before interpolation.
-            band0=[0]
-            band1=[NBand-1]
-            # narrow down the band range to find the nearest neighbours for interpolation.
-            for iBand in DicoCoefs.keys():
-                if DicoCoefs[iBand]["f0"]>=f:
-                    band1.append(iBand)
-                if DicoCoefs[iBand]["f0"]<=f:
-                    band0.append(iBand)
-            band0,band1=np.max(band0),np.min(band1)
-            xNull[ich]=100.
-            yNull[ich]=0.0036
-            chanBand0[ich]=band0
-            chanBand1[ich]=band1
-            NoMatch=False
-            if NoMatch:
-                raise NotImplementedError("The NenuFAR beam is not modeled for that frequency (channel %i @ %.3f MHz)"%(self.ATCABeamType,ich,ChanFreqs[ich]/1e6))
-        self.DicoCoefs=DicoCoefs
-        self.chanBand0=chanBand0
-        self.chanBand1=chanBand1
-        self.xNull=xNull
-        self.yNull=yNull
-        self.beamfreq0=beamfreq0
-
-
-    def evaluateBeam(self,*args,**kwargs):		
-        return self.GiveInstrumentBeam(*args,**kwargs)
-    
-    def GiveRawBeam(self,time,ra,dec):
-        nch=self.MS.ChanFreq.size
-        Beam=np.zeros((ra.shape[0],self.MS.na,self.MS.NSPWChan,2,2),dtype=np.complex)
-        rac,decc=self.MS.OriginalRadec
-        d=AngDist(ra,dec,rac,decc)*180./np.pi*60
-        DicoCoefs=self.DicoCoefs
-
-        for ich in range(nch):
-            bf0=self.beamfreq0
-            Dnu=d*self.MS.ChanFreq.flat[ich]*1e-9
-            band0=self.chanBand0[ich]
-            band1=self.chanBand1[ich]
-            Dnu0=d*DicoCoefs[band0]["f0"]*1e-9
-            Dnu1=d*DicoCoefs[band1]["f0"]*1e-9
-
-            if self.ATCABeamType=="csiro":
-                deltafreq0=np.abs(self.MS.ChanFreq.flat[ich]-DicoCoefs[band0]["f0"])
-                deltafreq1=np.abs(self.MS.ChanFreq.flat[ich]-DicoCoefs[band1]["f0"])
-                if deltafreq0==0 and deltafreq1==0:
-                    # This case does not require interpolation
-                    B=np.sqrt(np.abs(np.polynomial.polynomial.polyval(Dnu,np.array(DicoCoefs[band0]["C"]))))
-                    self.yNull[ich]=np.sqrt(np.abs(np.polynomial.polynomial.polyval(100.,np.array(DicoCoefs[band0]["C"]))))
-                else:
-                    # Perform interpolation
-                    beam0=np.sqrt(np.abs(np.polynomial.polynomial.polyval(Dnu0,np.array(DicoCoefs[band0]["C"]))))
-                    beam1=np.sqrt(np.abs(np.polynomial.polynomial.polyval(Dnu1,np.array(DicoCoefs[band1]["C"]))))
-                    B = (deltafreq0*beam0 + deltafreq1*beam1) / (deltafreq0 + deltafreq1)
-                    beam0=np.sqrt(np.abs(np.polynomial.polynomial.polyval(100.,np.array(DicoCoefs[band0]["C"]))))
-                    beam1=np.sqrt(np.abs(np.polynomial.polynomial.polyval(100.,np.array(DicoCoefs[band1]["C"]))))
-                    self.yNull[ich] = (deltafreq0*beam0 + deltafreq1*beam1) / (deltafreq0 + deltafreq1)
-                B[Dnu>self.xNull[ich]]=self.yNull[ich]/( (Dnu[Dnu>self.xNull[ich]]-self.yNull[ich]) )
-            B=B.reshape((-1,1))
-            Beam[:,:,ich,0,0]=B[:,:]
-            Beam[:,:,ich,1,1]=B[:,:]
-        
-
-        return Beam
-
-    def GiveInstrumentBeam(self,*args,**kwargs):
-        
+    def GiveInstrumentBeam(self,time,ras,decs):
+        # DDF internal: I assume this times the call to the beam for logging purposes
         T=ClassTimeIt.ClassTimeIt("GiveInstrumentBeam")
-        T.disable()
-        Beam=self.GiveRawBeam(*args,**kwargs)
-        nd,na,nch,_,_=Beam.shape
+        # get number of directions for later iterations
+        nd=len(ras)
+        # initialise internal beam matrix shape
+        Beam=np.zeros((nd,self.MS.na,self.MS.NSPWChan,2,2),dtype=complex)
+        ### convert time to astropy object with units of mjd for nenupy use
+        time=Time(time/24./3600,format="mjd",scale="utc")
+        ### create skycoord object of pointing direction: phase centre of MS
+        obs_coordinates=SkyCoord(self.MS.OriginalRadec[0],self.MS.OriginalRadec[1],unit="rad")
+        obs_coords=FixedTarget(obs_coordinates)
+        ### intrinsic dt of nenufar pointing: 6min. try to encode that somehow. Currently use 1s TODO
+        pointing=Pointing.target_tracking(target=obs_coords,
+                                          time=time,
+                                          duration=TimeDelta(1, format="sec"))
+        ### calculate frequencies
+        freqs=self.MS.ChanFreq.flatten()*u.Hz
+        ### initialise nenufar configuration to account for beam squint
+        conf = NenuFAR_Configuration(beamsquint_correction=True)
+        ### get sky coordinates at which to estimate nenufar beam
+        # XX polarisation
+        beam_coords_XX=Sky(SkyCoord(ras*u.rad,decs*u.rad,frame="icrs").ravel(),time=time,frequency=freqs,polarization=Polarization.NW)
+        # YY polarisation
+        beam_coords_YY=Sky(SkyCoord(ras*u.rad,decs*u.rad,frame="icrs").ravel(),time=time,frequency=freqs,polarization=Polarization.NE)
+        ### there are only 6 mini-array rotations in the NenuFAR array. Initialise them
+        ma_list=self.MS.StationNames
+        rotations = np.arange(0, 60, 10)
+        ma_rotated_like = list(map(miniarrays_rotated_like, rotations.reshape(6, 1)))
+        available_rotations = dict(zip(rotations.astype(str), ma_rotated_like))
+        # beams
+        beam_rot = {}
+        for rotation, mas in available_rotations.items():
+            # loop 6 iterations
+            ma = MiniArray(index=mas[0])
+            ### calculate beam response
+            # calculate beam. daskarray.value.compute() returns a np.array from a np.darray
+            beamvals_XX=ma.beam(sky=beam_coords_XX,pointing=pointing,configuration=conf).value.compute()
+            beamvals_YY=ma.beam(sky=beam_coords_YY,pointing=pointing,configuration=conf).value.compute()
+            # normalise - units are arbitrary and so are we
+            beamvals_XX=beamvals_XX/np.max(beamvals_XX)
+            beamvals_YY=beamvals_YY/np.max(beamvals_YY)
+            beam_rot[rotation] = np.array([beamvals_XX,beamvals_YY])
+        ### assign the appropriate rotations to the mini-arrays in the current observation
+        for i, ma in enumerate(ma_list):
+            ma_index = int(ma.strip("MR").strip("NEN"))
+            rotation_key = [key for key, val in available_rotations.items() if ma_index in val][0]
+            #gros_tableau[i, ....] = beam_rot["rotation_key"]
+            ### reshape to DDF internal beam matrix
+            for idir in range(nd):
+                # for polarisations: a prioi NW is XX, NE is YY
+                Beam[idir,i,:,0,0]=beam_rot[rotation_key][0][0,:,0,idir] # polarisation 1
+                Beam[idir,i,:,1,1]=beam_rot[rotation_key][1][0,:,0,idir] # polarisation 2
+
+        #print("End of debug")
+        #stop
+
         T.timeit("0")
-        MeanBeam=np.zeros((nd,na,self.NChanJones,2,2),dtype=Beam.dtype)
-        #L_ichZero=[]
+        MeanBeam=np.zeros((nd,self.MS.na,self.NChanJones,2,2),dtype=Beam.dtype)
         for ich in range(self.NChanJones):
             indCh=np.where(self.VisToJonesChanMapping==ich)[0]
-            # if indCh.size==0:
-            #     L_ichZero.append(ich)
-            #     continue
-            
             MeanBeam[:,:,ich,:,:]=np.mean(Beam[:,:,indCh,:,:],axis=2)
-            
         T.timeit("1")
 
         return MeanBeam
