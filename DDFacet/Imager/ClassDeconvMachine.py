5629ca3170f496a573ad24117259e717f1325849
cyriltasse <cyril.tasse@yahoo.fr>
2024-12-09 13:36:11 +0100

    ok

--- DDFacet/Imager/ClassDeconvMachine.py
+++ DDFacet/Imager/ClassDeconvMachine.py
@@ -531,11 +531,11 @@ class ClassImagerDeconv():
             
         if save and self._psfmean is not None:
             cycle_label = ".%02d"%cycle if cycle else ""
-            if "p" in self._saveims:
+            if "p" in self._saveims or "P" in self._saveims:
                 FacetMachinePSF.ToCasaImage(self._psfmean, ImageName="%s%s.psf" % (self.BaseName, cycle_label),
                                             Fits=True, beam=self.FWHMBeamAvg,
                                             Stokes=self.VS.StokesConverter.RequiredStokesProducts())
-            if "P" in self._saveims:
+
                 NormImage=self.DicoImagesPSF["JonesNorm"]
                 PSFCorr = self.DicoImagesPSF["ImageCube"]/np.sqrt(NormImage)
                 FacetMachinePSF.ToCasaImage(PSFCorr, ImageName="%s%s.psf.corr" % (self.BaseName, cycle_label),
